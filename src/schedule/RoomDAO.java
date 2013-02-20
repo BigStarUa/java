5f9d316bacdf6b81e455987a9d23beb99c4cad6c
BigStarUa <hleborezk@bigmir.net>
2013-02-21 01:33:47 +0200

    init home

--- src/schedule/RoomDAO.java
+++ src/schedule/RoomDAO.java
@@ -1,11 +1,27 @@
 package schedule;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class RoomDAO {
 
+	private final Connection con;
+	
 	public RoomDAO(Connection con)
 	{
+		this.con = con;
+	}
+	
+	private Room getGroupFromRS(ResultSet rs) throws SQLException 
+	{
+		Room room = new Room();
+		room.setId(rs.getInt("id"));
+		room.setName(rs.getString("name"));
+		room.setValue(rs.getInt("value"));
+		room.setCapacity(rs.getInt("capacity"));
+		
+		return room;
 		
 	}
 }
